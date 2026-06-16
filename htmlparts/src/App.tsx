8fb5025cd6ccd72ed0b38d9a4c653b1e71a3b384
kovacsdongo <kovacsdani04@gmail.com>
2026-06-16 18:04:43 +0200

    add eperdekor

---
+++ htmlparts/src/App.tsx
@@ -0,0 +1,12 @@
+import { Feature } from '@/components/ui/feature-with-image-comparison'
+import './App.css'
+
+function App() {
+  return (
+    <div className="w-full max-w-5xl mx-auto px-4">
+      <Feature />
+    </div>
+  )
+}
+
+export default App
