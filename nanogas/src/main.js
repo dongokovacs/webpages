63bbfad1130d4132fcf58a00cfe30f9d961e2628
kovacsdongo <kovacsdani04@gmail.com>
2026-08-26 08:22:38 +0200

    Revert "form magyar"
    This reverts commit 7f4597ddae8909ea0bcab84afb5e7442c0ab8d62.

--- nanogas/src/main.js
+++ nanogas/src/main.js
@@ -50,7 +50,7 @@
   const bookingForm = document.getElementById("booking-form");
   const bookingStatus = document.getElementById("booking-form-status");
 
-  const inquiryType = document.getElementById("erdeklodes-tipusa");
+  const inquiryType = document.getElementById("inquiry-type");
   if (inquiryType) {
     const conditionalFields = document.querySelectorAll("[data-show-for]");
     const requireFields = document.querySelectorAll("[data-require-for]");
